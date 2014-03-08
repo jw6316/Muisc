a22d6a1486b30ad0491e8b427bd3acc502722a81
Jamie Walker <Jamie@Macintosh-2.local>
2014-03-08 17:24:32 +0900

    Initial Commit

---
+++ MusicPlayer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MusicPlayer
+//
+//  Created by Jamie Walker on 3/8/14.
+//  Copyright (c) 2014 Jamie Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MusicPlayer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MusicPlayer
+//
+//  Created by Jamie Walker on 3/8/14.
+//  Copyright (c) 2014 Jamie Walker. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
